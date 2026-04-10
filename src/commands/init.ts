f86316741c9ee2d8aa2af125b30eea682d33f49d
Lucas Araujo <contatodevaraujo@gmail.com>
2026-04-10 17:22:14 -0300

    chore: setup eslint, prettier and vitest for code quality and testing

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -1,9 +1,9 @@
 import { Command } from "commander";
 import chalk from "chalk";
 
-export const init = new Command('init')
-  .description('initialize a new nextjs project')
-  .argument('<name>', 'project name')
+export const init = new Command("init")
+  .description("initialize a new nextjs project")
+  .argument("<name>", "project name")
   .action(async (name: string) => {
     console.log(chalk.green(`initializing project: ${name}`));
   });
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,15 @@
-import { Command } from 'commander';
-import { init } from '@/commands/init';
+import { Command } from "commander";
+import { init } from "@/commands/init";
 
 async function main() {
   const program = new Command();
 
   program
-    .name('kitraw')
-    .description('a modern generator for nextjs projects')
-    .version('1.0.0');
+    .name("kitraw")
+    .description("a modern generator for nextjs projects")
+    .version("1.0.0");
 
-  program
-    .addCommand(init)
+  program.addCommand(init);
 
   program.parse();
 }
