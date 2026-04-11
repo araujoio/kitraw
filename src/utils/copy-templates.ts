1dbb5d4a65eca3e9f5baa8ea3474670c5f7cc64f
Lucas Araujo <contatodevaraujo@gmail.com>
2026-04-11 18:09:13 -0300

    refactor: add typing to const declarations

--- src/utils/copy-templates.ts
+++ src/utils/copy-templates.ts
@@ -1,8 +1,8 @@
 import { FileSystem } from "@/lib/file-system";
 import path from "path";
 
-const fileSystem = new FileSystem();
-const templatePath = path.join(process.cwd(), "src", "templates");
-const distPath = path.join(process.cwd(), "dist", "templates");
+const fileSystem: FileSystem = new FileSystem();
+const templatePath: string = path.join(process.cwd(), "src", "templates");
+const distPath: string = path.join(process.cwd(), "dist", "templates");
 
 fileSystem.copy(templatePath, distPath);
