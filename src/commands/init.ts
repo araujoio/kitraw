1821ec1e1350ffddb2b84d67729bb0377cb0dcdd
Lucas Araujo <contatodevaraujo@gmail.com>
2026-04-19 08:16:34 -0300

    feat(init): use service for project initialization

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -1,9 +1,24 @@
 import { Command } from "commander";
+import { InitService } from "@/services/init-service";
 import chalk from "chalk";
 
 export const init = new Command("init")
+  .name("init")
+  .alias("create")
   .description("initialize a new nextjs project")
-  .argument("<name>", "project name")
+  .argument("<name>", "the name for the new project.")
   .action(async (name: string) => {
-    console.log(chalk.green(`initializing project: ${name}`));
+    const initService = new InitService();
+
+    try {
+      await initService.initializeProject(name);
+      console.log((`Success! Created ${name} at: ${chalk.greenBright(process.cwd() + "/" + name)}`));
+    } catch (err) {
+      if (err instanceof Error) {
+        console.error(chalk.red(err.message));
+      } else {
+        console.error(chalk.red("Unexpected error ocurred:"), err);
+      }
+      process.exit(1);
+    }
   });
