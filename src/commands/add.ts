6d89eb221e4cdb6e76432901395b01e7d5df18cf
Lucas Araujo <contatodevaraujo@gmail.com>
2026-05-02 20:43:15 -0300

    refactor(commands): use path alias for createPage import in add command

--- src/commands/add.ts
+++ src/commands/add.ts
@@ -1,5 +1,5 @@
 import { Command } from "commander";
-import { createPage } from "./create-page";
+import { createPage } from "@/commands/create-page";
 
 export const add = new Command("add")
   .description("add resources to your project")
