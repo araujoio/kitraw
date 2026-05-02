08a2b5c6b7416dce42c04e9dcd61ca710796c5fb
Lucas Araujo <contatodevaraujo@gmail.com>
2026-05-01 23:22:28 -0300

    style(templates): format home page content

--- src/templates/next-app/src/app/[locale]/page.tsx
+++ src/templates/next-app/src/app/[locale]/page.tsx
@@ -1,10 +1,10 @@
 import { useTranslations } from "next-intl";
 
 export default function HomePage() {
-    const t = useTranslations("Index");
-    return (
-        <div>
-            <h1>{t("title")}</h1>
-        </div>
-    )
+  const t = useTranslations("Index");
+  return (
+    <div>
+      <h1>{t("title")}</h1>
+    </div>
+  )
 }
